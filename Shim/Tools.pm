e0a522df76597345f1cd3b5af06a881feb8bda42
goodind1 <drew@goodin.ignorelist.com>
2017-02-26 09:41:27 -0600

    script that uses post objects replaces old post component view

--- Shim/Tools.pm
+++ Shim/Tools.pm
@@ -5,18 +5,20 @@ use warnings;
 our @EXPORT = qw!fetch_posts extract_title!;
 
 sub fetch_posts {
-    my $dir = $ENV{DOCUMENT_ROOT} . qq?/posts/?;
+    my $dir = $ENV{DOCUMENT_ROOT} . qq?/nowrap/posts/?;
     opendir my $dh, $dir;
     my @posts = grep { /^[0-9]+$/ } readdir $dh;
     my @post_objs = map { Shim::Post->new($_) } @posts;
     return \@post_objs;
 }
 
-sub extract_title {
+sub post_content { #returns list containing title and aref of body lines
     my $id = shift;
-    open my $fh, '<', $ENV{DOCUMENT_ROOT} . qq?/posts/$id?;
+    open my $fh, '<', $ENV{DOCUMENT_ROOT} . qq?/nowrap/posts/$id?;
     my @post_lines = <$fh>;
-    return shift @post_lines;
+    my $title = shift @post_lines;
+    chomp $title;
+    return (title => $title, body => \@post_lines);
 }
 
 1;
