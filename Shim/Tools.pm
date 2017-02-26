1e142256c8624d6126477e703b116cd3972f6ef0
goodind1 <drew@goodin.ignorelist.com>
2017-02-26 13:39:42 -0600

    lines from post files should be stripped of their carriage returns before markdown

--- Shim/Tools.pm
+++ Shim/Tools.pm
@@ -6,7 +6,7 @@ our @EXPORT = qw!fetch_posts extract_title!;
 
 sub fetch_posts {
     my $dir = $ENV{DOCUMENT_ROOT} . qq?/nowrap/posts/?;
-    opendir my $dh, $dir;
+    opendir (my $dh, $dir);
     my @posts = grep { /^[0-9]+$/ } readdir $dh;
     my @post_objs = map { Shim::Post->new($_) } @posts;
     return \@post_objs;
